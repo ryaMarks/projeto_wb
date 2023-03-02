5973897a7e614643f87eaa0d544624ee68e6031c
Ryam Marques <ryab08340gmail.com>
2023-03-02 19:12:58 -0300

    arquivos estaticos adicionados

--- wb_instalacoes/urls.py
+++ wb_instalacoes/urls.py
@@ -1,6 +1,6 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LogoutView
 from django.urls import include, path
 
 urlpatterns = [
@@ -11,4 +11,4 @@ urlpatterns = [
     path('produto/', include('wb_instalacoes.produto.urls')),
     path('estoque/', include('wb_instalacoes.estoque.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
